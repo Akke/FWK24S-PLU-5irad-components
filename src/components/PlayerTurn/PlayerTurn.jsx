a5c7cc9b9a18aa67d2afb2f0ec5836e4728bdcce
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-01 10:31:21 +0200

    added username prop

--- src/components/PlayerTurn/PlayerTurn.jsx
+++ src/components/PlayerTurn/PlayerTurn.jsx
@@ -1,9 +1,9 @@
 import "./PlayerTurn.css";
 
-const PlayerTurn = () => {
+const PlayerTurn = ({ username = "Player X" }) => {
     return (
         <div className="player-turn">
-            <div className="player-name">Player X</div>
+            <div className="player-name">{username}</div>
             <div className="description">Make your move</div>
         </div>
     );
