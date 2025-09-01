b0b423d11bb1043b353167b5f53b89447d1695b9
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-01 10:30:08 +0200

    added PlayerTurn.jsx Component

---
+++ src/components/PlayerTurn/PlayerTurn.jsx
@@ -0,0 +1,12 @@
+import "./PlayerTurn.css";
+
+const PlayerTurn = () => {
+    return (
+        <div className="player-turn">
+            <div className="player-name">Player X</div>
+            <div className="description">Make your move</div>
+        </div>
+    );
+}
+
+export default PlayerTurn;
---
+++ src/components/PlayerTurn/PlayerTurn.stories.jsx
@@ -0,0 +1,11 @@
+import PlayerTurn from './PlayerTurn';
+
+const meta = {
+  component: PlayerTurn,
+};
+
+export default meta;
+
+export const Default = {
+  args: {}
+};
