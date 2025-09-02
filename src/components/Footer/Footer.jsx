eb158fde76c135c6dda76b6b528fe72c0a70413f
angelika-friis <angelikafriis@gmail.com>
2025-09-02 15:47:47 +0200

    added footer [#61]

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Made with 🖤 by</p>
+            <p>Andreas, Angelika, Axel, Gul och Hampus</p>
+        </footer>
+    )
+}
+export default Footer;
