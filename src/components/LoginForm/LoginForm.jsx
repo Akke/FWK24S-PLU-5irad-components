b306a8705d0017a0dd3fa4d0f7faa67e55d28bdb
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-03 10:05:58 +0200

    restructured LoginForm as test for new structure

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,8 @@
-import "./LoginForm.css";
+import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
     return (
-        <form action="POST" className="login-form">
+        <form action="POST" className={styles["login-form"]}>
             <input type="text" name="username" placeholder="Username" />
             <input type="password" name="password" placeholder="Password" />
             <button type="submit">Login</button>
