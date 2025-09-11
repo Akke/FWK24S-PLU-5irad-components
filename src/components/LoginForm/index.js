564bc7ceb6f8f2f09e8647c9934a8493879f4bb5
Akke <sirpurpleness@gmail.com>
2025-09-11 11:32:02 +0200

    added package.json, added barrel file

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,4 @@
+export { default as Button } from "./Button";
+export { default as LoginForm } from "./LoginForm";
+export { default as Password } from "./Password";
+export { default as Username } from "./Username";
