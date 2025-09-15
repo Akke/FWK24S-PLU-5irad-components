f3f3b56c458f9fba400fd89c3c6a47fe81e53c80
angelika-friis <angelikafriis@gmail.com>
2025-09-15 11:22:31 +0200

    added mouse over sound for boardSquare

--- src/components/BoardComponents/BoardSquare/BoardSquare.jsx
+++ src/components/BoardComponents/BoardSquare/BoardSquare.jsx
@@ -1,9 +1,13 @@
-import styles from "./BoardSquare.module.css"
-import PlayerToken from "../PlayerToken/PlayerToken"
+import styles from "./BoardSquare.module.css";
+import PlayerToken from "../PlayerToken/PlayerToken";
+import { useSound } from 'react-sounds';
+import customSound from '../../../assets/sounds/166186__drminky__menu-screen-mouse-over.wav';
 
 export const BoardSquare = ({ tile = 0 }) => {
+    const { play } = useSound(customSound);
+
     return (
-        <div className={styles.boardSquareContainer}>
+        <div className={styles.boardSquareContainer} onMouseOver={() => play()}>
             {tile !== 0 ?
                 <div className={styles.coinContainer}>
                     <PlayerToken tile={tile} />
