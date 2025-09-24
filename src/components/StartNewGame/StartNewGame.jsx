4cfff73c26e765ed126abf2d3e1826a3c2f9efb1
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-24 21:39:38 +0200

    styled lobby page

--- src/components/StartNewGame/StartNewGame.jsx
+++ src/components/StartNewGame/StartNewGame.jsx
@@ -8,13 +8,13 @@ const StartNewGame = ({ onRedirect }) => {
     }
     return(
         <div className={style.createNewGameContainer}>
-            <h1 className={style.createGameHeader}>Start Game</h1>
             <form onSubmit={(e)=>handleSubmit(e)} className={style.createNewGameForm}>
                 <input className={style.createNewGameInput}type='text' placeholder='Game Lobby ID'/>
                 <button className={style.joinGameButton} type='submit'>Join</button>
             </form>
-            <hr className={style.divider}/>
-            <CreateNewGameButton onRedirect={onRedirect} />
+            <div className={style.newGameButton}>
+                <CreateNewGameButton onRedirect={onRedirect} />
+            </div>
         </div>
     )
 }
