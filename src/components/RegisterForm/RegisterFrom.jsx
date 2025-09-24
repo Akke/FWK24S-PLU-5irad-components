a9514bfca6e4caaab4f9a73499a0c37b46c129d7
angelika-friis <angelikafriis@gmail.com>
2025-09-24 15:50:26 +0200

    separation of concern for RegisterForm

--- src/components/RegisterForm/RegisterFrom.jsx
+++ src/components/RegisterForm/RegisterFrom.jsx
@@ -1,18 +1,13 @@
 import styles from './RegisterForm.module.css'
 
-const RegisterForm = () => {
-    const onSubmit = (e) => {
-        e.preventDefault()
-
-        console.log('registration from submitted')
-    }
-    return(
+const RegisterForm = ({ onSubmit, setUsername, setPassword }) => {
+    return (
         <>
             <div className={styles['register-form']}>
                 <h1>Register</h1>
-                <form action='POST' onSubmit={onSubmit}>
-                    <input type='text' name='username' placeholder='Username'/>
-                    <input type='password' name='password' placeholder='Password'/>
+                <form onSubmit={onSubmit}>
+                    <input type='text' name='username' placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
+                    <input type='password' name='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
                     <button type='submit'>Register</button>
                 </form>
             </div>
