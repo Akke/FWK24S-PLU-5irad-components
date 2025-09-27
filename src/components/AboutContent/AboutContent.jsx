a03fa0de33ae816177f89fc462477bd945c72c9f
Hampus von Horn <hampus.vh@gmail.com>
2025-09-27 21:53:22 +0200

    AboutContent moved from frontend to components

---
+++ src/components/AboutContent/AboutContent.jsx
@@ -0,0 +1,18 @@
+import styles from "./AboutContent.module.css";
+
+const AboutContent = () => (
+    <section className={styles.aboutContent}>
+        <h1>Our vision</h1>
+        <p>This project started with a simple idea:</p>
+        <blockquote>
+            "As a player, I want to play a game of Gomoku so that I can win, lose or draw."
+        </blockquote>
+        <p>
+            That's it. Just a classic five-in-a-row game, brought online so we can
+            challenge each other and have some fun.
+        </p>
+        <hr className={styles.hr} />
+    </section>
+);
+
+export default AboutContent;
