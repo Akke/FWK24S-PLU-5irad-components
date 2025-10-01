6c8d2459cec5ebfb9a118da81a3db672d1be6e49
angelika-friis <angelikafriis@gmail.com>
2025-10-01 20:50:56 +0200

    Switched WinScreen colors to varibles and made message a prop

--- src/components/WinScreen/WinScreen.jsx
+++ src/components/WinScreen/WinScreen.jsx
@@ -1,13 +1,13 @@
 import styles from './WinScreen.module.css'
 import RestartGameButton from '../RestartGameButton/RestartGameButton'
 
-const WinScreen = () => {
+const WinScreen = ({ message }) => {
     return (
         <div className={styles.overlay}>
             <div className={styles.container}>
                 <button className={styles.button}>✖</button>
                 <div className={styles.content}>
-                    <h1 className={styles.text}>You won!</h1>
+                    <h1 className={styles.text}>{message}</h1>
                     <RestartGameButton className={styles.restartBtn} />
                 </div>
             </div>
--- src/components/WinScreen/WinScreen.stories.js
+++ src/components/WinScreen/WinScreen.stories.js
@@ -1,11 +1,20 @@
 import WinScreen from "./WinScreen";
 
 const meta = {
+    name: "components/EndScreen",
     component: WinScreen,
 }
 
 export default meta
 
 export const Default = {
-    args: {}
+    args: { message: "You won!" }
+}
+
+export const Lose = {
+    args: { message: "You lost" }
+}
+
+export const Draw = {
+    args: { message: "It's a draw" }
 }
