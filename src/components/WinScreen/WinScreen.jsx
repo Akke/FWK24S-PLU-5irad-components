453112ec18f97e9524846290a55a72502dd3b31f
angelika-friis <angelikafriis@gmail.com>
2025-10-02 09:28:34 +0200

    Change background in GameInfoBox

--- src/components/WinScreen/WinScreen.jsx
+++ src/components/WinScreen/WinScreen.jsx
@@ -1,14 +1,14 @@
 import styles from './WinScreen.module.css'
 import RestartGameButton from '../RestartGameButton/RestartGameButton'
 
-const WinScreen = ({ message, handleClose }) => {
+const WinScreen = ({ message, handleClose, handleRestart }) => {
     return (
         <div className={styles.overlay}>
             <div className={styles.container}>
                 <button className={styles.button} onClick={handleClose}>✖</button>
                 <div className={styles.content}>
                     <h1 className={styles.text}>{message}</h1>
-                    <RestartGameButton className={styles.restartBtn} />
+                    <RestartGameButton className={styles.restartBtn} onClick={handleRestart} />
                 </div>
             </div>
         </div>
